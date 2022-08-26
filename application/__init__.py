85bb39823a062dd48d7bc43d5067eab2cc539e85
vishnu <vishnu.m@pacewisdom.com>
2022-08-26 11:32:37 +0530

    showing user data

--- application/__init__.py
+++ application/__init__.py
@@ -1,6 +1,13 @@
+from distutils.command.config import config
 from flask import Flask
+from config import Config
+from flask_mongoengine import MongoEngine
 
 app = Flask(__name__)#special variable to identify the current application or module that is being rendered or passed to flask
 
+app.config.from_object(Config)
+
+db = MongoEngine()
+db.init_app(app)
 from application import routes
 
