48e16b600bd240d6d58f27a00712d41338e6803a
vishnu <vishnu.m@pacewisdom.com>
2022-09-28 00:07:35 +0530

    changes updated

--- application/routes.py
+++ application/routes.py
@@ -1,50 +1,55 @@
-from distutils import core
-import email
-import pymongo
-from pickle import GET
-from unicodedata import category
-from application.froms import LoginForm, RegisterFrom
-from application.models import User, Course, Enrollment
-from flask import request, json, Response, flash, redirect, url_for, session
 from application import app, db
-from flask import render_template
+from flask import render_template, request, json, Response, redirect, flash, url_for, session
+from application.models import User, Course, Enrollment
+from application.forms import LoginForm, RegisterForm
 
 courseData = [{"courseID":"1111","title":"PHP 111","description":"Intro to PHP","credits":"3","term":"Fall, Spring"}, {"courseID":"2222","title":"Java 1","description":"Intro to Java Programming","credits":"4","term":"Spring"}, {"courseID":"3333","title":"Adv PHP 201","description":"Advanced PHP Programming","credits":"3","term":"Fall"}, {"courseID":"4444","title":"Angular 1","description":"Intro to Angular","credits":"3","term":"Fall, Spring"}, {"courseID":"5555","title":"Java 2","description":"Advanced Java Programming","credits":"4","term":"Fall"}]
 
 @app.route("/")
-@app.route('/index')
+@app.route("/index")
+@app.route("/home")
 def index():
-    return render_template("index.html", index=True)
+    return render_template("index.html", index=True )
 
-@app.route('/login', methods=['GET','POST'])
+@app.route("/login", methods=['GET','POST'])
 def login():
+    if session.get('username'):
+        return redirect(url_for('index'))
+
     form = LoginForm()
     if form.validate_on_submit():
-        email = form.email.data
-        password = form.password.data
-        
+        email       = form.email.data
+        password    = form.password.data
+
         user = User.objects(email=email).first()
-        
-        if user and user.get_password(password): 
-            flash(f"{user.first_name}, You're successfully logged in", "success")
+        if user and user.get_password(password):
+            flash(f"{user.first_name}, you are successfully logged in!", "success")
+            session['user_id'] = user.user_id
+            session['username'] = user.first_name
             return redirect("/index")
         else:
-            flash("Something went wrong","danger")
-    return render_template("login.html",title="Login", form=form, login=True)
+            flash("Sorry, something went wrong.","danger")
+    return render_template("login.html", title="Login", form=form, login=True )
 
-@app.route('/courses')
-@app.route('/courses/<term>')
-def courses(term=None):
+@app.route("/logout")
+def logout():
+    session['user_id']=False
+    session.pop('username',None)
+    return redirect(url_for('index'))
+
+@app.route("/courses/")
+@app.route("/courses/<term>")
+def courses(term = None):
     if term is None:
         term = "Spring 2019"
-    classes = Course.objects.order_by("+courseID")
-    return render_template("courses.html", courseData=classes, courses=True, term=term)
+    classes = Course.objects.order_by("-courseID")
+    return render_template("courses.html", courseData=classes, courses = True, term=term )
 
 @app.route("/register", methods=['POST','GET'])
 def register():
-    # if session.get('username'):
-    #     return redirect(url_for('index'))
-    form = RegisterFrom()
+    if session.get('username'):
+        return redirect(url_for('index'))
+    form = RegisterForm()
     if form.validate_on_submit():
         user_id     = User.objects.count()
         user_id     += 1
@@ -60,12 +65,14 @@ def register():
         flash("You are successfully registered!","success")
         return redirect(url_for('index'))
     return render_template("register.html", title="Register", form=form, register=True)
-    
+
+
+
 @app.route("/enrollment", methods=["GET","POST"])
 def enrollment():
 
-    # if not session.get('username'):
-    #     return redirect(url_for('login'))
+    if not session.get('username'):
+        return redirect(url_for('login'))
 
     courseID = request.form.get('courseID')
     courseTitle = request.form.get('title')
@@ -116,21 +123,25 @@ def enrollment():
             }
         ]))
 
-    return render_template("enrollment.html", enrollment=True, title="Enrollment", classes=classes)
+    return render_template("enrollment.html", enrollment=True, title="Enrollment", classes=classes)    
 
-@app.route('/api/')
-@app.route('/api/<idx>')
+
+
+
+@app.route("/api/")
+@app.route("/api/<idx>")
 def api(idx=None):
     if(idx == None):
         jdata = courseData
     else:
         jdata = courseData[int(idx)]
-    return Response(json.dumps(jdata), mimetype = "application/json")
     
-    
-@app.route('/user')
+    return Response(json.dumps(jdata), mimetype="application/json")
+
+
+@app.route("/user")
 def user():
-    # User(user_id=1, first_name="Vishnu", last_name="Mohan", email="vishnu@gmail.com", password="262q821882").save()
-    # User(user_id=2, first_name="Mary", last_name="Jane", email="jane@gmail.com", password="162dsds182").save()
-    users = User.objects.all()
-    return render_template("user.html", users = users)
+     #User(user_id=1, first_name="Christian", last_name="Hur", email="christian@uta.com", password="abc1234").save()
+     #User(user_id=2, first_name="Mary", last_name="Jane", email="mary.jane@uta.com", password="password123").save()
+     users = User.objects.all()
+     return render_template("user.html", users=users)
